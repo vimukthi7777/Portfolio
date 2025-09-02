036b8570b14926271c55fb53d6e9b666cd6dedf9
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-02 21:08:08 +0530

    feat: finalize navigation section with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-auto'>
       <Navbar />
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
     </div>
   )
 }
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,5 +1,7 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { socials } from '../constants';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Navbar = () => {
     //Gsap
@@ -8,6 +10,86 @@ const Navbar = () => {
     const contactRef = useRef(null);
     const topLineRef = useRef(null);
     const bottomLineRef = useRef(null);
+    const tl = useRef(null);
+    const iconTl = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const [showBurger, setShowBurger] = useState(true);
+
+    useGSAP(() => {
+        gsap.set(navRef.current, { xPercent:100});
+        gsap.set([linksRef.current, contactRef.current], {
+            autoAlpha: 0,
+            x: -20,
+        });
+
+        tl.current = gsap.timeline({paused:true}).to(navRef.current, {
+            xPercent:0,
+            duration: 1,
+            ease: "power3.out",
+        }).to(
+            linksRef.current,
+            {
+                autoAlpha:1,
+                x:0,
+                stagger: 0.1, //0.1s after previous one
+                duration:0.5,
+                ease: "power2.out",
+            },
+            "<" //start animation at the same time with first one
+        ).to(contactRef.current, 
+            {
+                autoAlpha:1,
+                x: 0,
+                duration: 0.5,
+                ease: "power2.out",
+            }, 
+            "<+0.2" // 0.2s after previous animation
+    );
+
+        iconTl.current = gsap
+        .timeline({paused: true}).to(
+            topLineRef.current,{
+                rotate: 45,
+                y: 3.3,
+                duration: 0.3,
+                ease: "power2.inOut",
+            }
+        ).to(
+            bottomLineRef.current, {
+                rotate: -45,
+                y:-3.3,
+                duration: 0.3,
+                ease: "power2.inOut",
+            }, "<"
+        );
+    },[]);
+
+    useEffect(() => {
+        let lastScrollY = window.scrollY;
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            setShowBurger(currentScrollY <= lastScrollY ||
+            currentScrollY < 10);
+
+            lastScrollY = currentScrollY;
+        };
+        window.addEventListener("scroll", handleScroll, {
+            passive: true,
+        });
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const toggleMenu = () => {
+        if (isOpen) {
+            tl.current.reverse();
+            iconTl.current.reverse();
+        }else {
+            tl.current.play();
+            iconTl.current.play();
+        }
+        setIsOpen(!isOpen);
+    };
   return (
     <>
     <nav 
@@ -43,7 +125,10 @@ const Navbar = () => {
             </div>
     </nav>
     <div className='fixed z-50 flex flex-col items-center justify-center gap-1 transition-all duration-300 bg-black 
-    rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20 top-4 right-10'>
+    rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20 top-4 right-10' onClick={toggleMenu} 
+    style={showBurger
+        ? {clipPath: "circle(50% at 50% 50%)"}
+        : {clipPath: "circle(0% at 50% 50%)"} } >
         <span ref={topLineRef} className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
         <span ref={bottomLineRef} className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
     </div>
