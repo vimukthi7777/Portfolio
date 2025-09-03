f7e835e6e1b8ab6f8a63430d2df0b65e04978365
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-03 15:52:47 +0530

    feat: start building services section

---
+++ src/sections/ServiceSummary.jsx
@@ -0,0 +1,17 @@
+
+const ServiceSummary = () => {
+  return (
+    <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
+        <div id="title-service-1">
+            <p>Architucture</p>
+        </div>
+        <div id="title-service-2" className="flex items-center justify-center gap-3 translate-x-16">
+            <p className="font-normal">Development</p>
+            <div className="w-10 h-1 md:w-32 bg-gold"/>
+            <p>Deployment</p>
+        </div>
+    </section>
+  )
+}
+
+export default ServiceSummary
