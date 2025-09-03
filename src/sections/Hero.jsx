2c08639004e0309d5663a572d22973e69c9ede29
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-03 10:14:01 +0530

    feat: add base structure of hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,34 @@
+import { useRef } from "react"
+
+const Hero = () => {
+    const contextRef = useRef(null);
+    const headerRef = useRef(null);
+  return (
+    <section
+        id="home"
+        className="flex flex-col justify-end min-h-screen">
+            <div ref={contextRef}>
+                <div style={{clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)"}}>
+                    <div
+                    ref={headerRef}
+                    className="flex flex-col justify-center gap-12 pt-16 sm:gap-16">
+                        <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">404 No Bugs Found</p>
+                        <div className="px-10">
+                            <h1 className="flex flex-col flex-wrap gap-12  text-black uppercase banner-text-responsive sm:gap-16 md:block">Vimukthi</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative px-10 text-black">
+                    <div className="absolute inset-x-0 border-t-2" />
+                    <div className="py-12 sm:py-16 text-end">
+                        <p className="font-light uppercase value-text-responsive">
+                            I help growing brands and startups gain an unfair advantage through premium results driven webs/apps
+                        </p>
+                    </div>
+                </div>
+            </div>
+    </section>
+  )
+}
+
+export default Hero
