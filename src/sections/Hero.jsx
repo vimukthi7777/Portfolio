7d17629a1d0ef42b8a00de254ff9f4b74f6dc9f5
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-05 14:02:00 +0530

    chore: update content in existing sections

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,9 +7,9 @@ import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 
 const Hero = () => {
     const isMobile = useMediaQuery({maxWidth:853});
-    const text = `I help growing brands and startups gain an
-                           unfair advantage through premium
-                           results driven webs/apps`;
+    const text = `I Design and Develop Web and Mobile Apps
+                          Crafting Intuitive Interfaces
+                          Bringing Ideas to Life`;
 
         
   return (
